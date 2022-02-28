7b376fded359b44fab63f687cd1e1969f02e6252
Shree Rai <shreerai1@outlook.com>
2022-02-28 09:41:27 +0545

    agg tables

--- country_source.py
+++ country_source.py
@@ -5,12 +5,17 @@ import util
 import pandas as pd
 from lib.Logger import Logger
 from lib.Variables import Variables
+from lib.Auditor import Auditor
+from lib.DWHNightlyExtractBatch import DWHNightlyExtractBatch
 
 conn = sc.connect(user=snow_flake_config.username, password=snow_flake_config.password,
                   account=snow_flake_config.account)
 variables = Variables("etc/ENV.cfg")
 variables.set("SCRIPT_NAME", "country_source")
+variables.set("BOOKMARK", "NONE")
 log = Logger(variables)
+audit = Auditor(variables, log, None)
+dim_module = DWHNightlyExtractBatch(None, variables, log, audit)
 
 
 def execute_query(connection, __query):
@@ -28,36 +33,38 @@ def fetch_from_source(__query):
 
 
 def compressed_to_csv(result, file_name):
-    result.to_csv(r"C:\Users\shrikrishna.rai\PycharmProjects\ETL_basics\csv\sales\{0}".format(file_name),
+    result.to_csv(r".\csv\sales\{0}".format(file_name),
                   index=False,
                   header=False)
 
 
 try:
-    query = 'use {}'.format("BHATBHATENI")
-    execute_query(conn, query)
-
-    query = 'use warehouse {}'.format("COMPUTE_WH")
-    execute_query(conn, query)
-
-    try:
-        query = 'alter warehouse {} resume'.format("COMPUTE_WH")
+    if variables.get("BOOKMARK") == "NONE":
+        query = 'use {}'.format("BHATBHATENI")
         execute_query(conn, query)
-    except Exception as e:
-        print(e)
-    compressed_to_csv(fetch_from_source("select * from country"), variables.get("SCRIPT_NAME"))
-    csv_file = r"C:\Users\shrikrishna.rai\PycharmProjects\ETL_basics\csv\sales\country"
-    query = 'put file://{0} @{1} auto_compress=true'.format(csv_file, "BHAT_BHATENI_STAGE")
-    log.log_message("uploaded compressed file to snoflake stage")
-    execute_query(conn, query)
 
-    log.log_message("truncate stage table")
-    execute_query(conn, "truncate bhatbhateni_stg.country")
+        query = 'use warehouse {}'.format("COMPUTE_WH")
+        execute_query(conn, query)
 
-    query = "copy into {0} from @{1}/country.gz FILE_FORMAT=(TYPE=csv field_delimiter='," \
-            "' skip_header=0)" \
-            "ON_ERROR ='ABORT_STATEMENT'".format("bhatbhateni_stg.country", "BHAT_BHATENI_STAGE")
-    execute_query(conn, query)
+        try:
+            query = 'alter warehouse {} resume'.format("COMPUTE_WH")
+            execute_query(conn, query)
+        except Exception as e:
+            print(e)
+        compressed_to_csv(fetch_from_source("select * from country"), variables.get("SCRIPT_NAME"))
+        csv_file = r"C:\Users\shrikrishna.rai\PycharmProjects\ETL_basics\csv\sales\country"
+        query = 'put file://{0} @{1} auto_compress=true'.format(csv_file, "BHAT_BHATENI_STAGE")
+        execute_query(conn, query)
+        log.log_message("Extracted File Transfer to Snowflake FILE_STG is complete")
+        dim_module.set_bookmark("FILE_COMPLETE")
+
+        log.log_message("truncate stage table")
+        execute_query(conn, "truncate bhatbhateni_stg.country")
+
+        query = "copy into {0} from @{1}/country.gz FILE_FORMAT=(TYPE=csv field_delimiter='," \
+                "' skip_header=0)" \
+                "ON_ERROR ='ABORT_STATEMENT'".format("bhatbhateni_stg.country", "BHAT_BHATENI_STAGE")
+        execute_query(conn, query)
 
 except Exception as e:
     print(e)
--- f_sales_tgt.py
+++ f_sales_tgt.py
@@ -1,5 +1,4 @@
 import snowflake.connector as sc
-from snowflake.connector import DictCursor
 
 from config import snow_flake_config
 from lib.Logger import Logger
@@ -25,12 +24,21 @@ try:
     query = 'use warehouse {}'.format("COMPUTE_WH")
     execute_query(conn, query)
 
+    query = """           insert into bhatbhateni_tgt.month_dim
+            (month_record) select month(transaction_time) from bhatbhateni_tmp.sales"""
+    execute_query(conn, query)
+    log.log_message("successfully loaded dimension")
+
     query = """
-            INSERT INTO bhatbhateni_tgt.f_sales
-            (id,store_id,product_id,customer_id,quantity,amount,discount,record_active)
-            select id,store_id,product_id,customer_id,quantity,amount,discount,1 from 
-            bhatbhateni_stg.sales
+            INSERT INTO bhatbhateni_tgt.F_BHATBHATENI_AGG_SLS_PLC_MONTH_T
+            (id,store_id,product_id,customer_id,quantity,amount,discount,record_active,month_id)
+            select id,store_id,product_id,customer_id,quantity,amount,discount,1,m.month_record
+             from bhatbhateni_tmp.sales s
+            inner join bhatbhateni_tgt.month_dim m on m.month_record = month(s.transaction_time)
     """
+
+    log.log_message("successfully loaded fact")
+
     execute_query(conn, query)
 
     log.log_message("insert to target table")
